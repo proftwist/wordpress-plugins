ff32f0c4fafd3e7aab5572eb2200188de4b0415d
Vladimir Bychko <vladimir@bychko.ru>
2025-11-13 16:30:46 +0200

    Исправление дефектов отображения диаграммы. Теперь она всегда включает последние дни, даже если это не воскресенье.

--- github-commit-chart/github-commit-chart.php
+++ github-commit-chart/github-commit-chart.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: GitHub Commit Chart
  * Description: Отображает диаграмму коммитов GitHub в виде Gutenberg-блока или шорткода
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: Владимир Бычко
  * Author URL: https://bychko.ru
  * Text Domain: github-commit-chart
