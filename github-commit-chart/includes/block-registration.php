a8dd24afed09d2612bd2a82ac82260c0a60f3719
Vladimir Bychko <vladimir@bychko.ru>
2025-11-13 04:51:17 +0200

    Багфиксы.

--- github-commit-chart/includes/block-registration.php
+++ github-commit-chart/includes/block-registration.php
@@ -23,17 +23,17 @@ function github_commit_chart_register_block() {
 function github_commit_chart_render_block($attributes, $content) {
     // Получаем имя пользователя из атрибутов блока или из глобальных настроек
     $github_profile = !empty($attributes['githubProfile']) ? $attributes['githubProfile'] : get_option('github_commit_chart_github_profile', '');
-    
+
     if (empty($github_profile)) {
         return '<p>Пожалуйста, укажите путь к профилю GitHub в настройках плагина или в атрибутах блока.</p>';
     }
-    
+
     // Генерируем уникальный ID для контейнера диаграммы
     $unique_id = uniqid('gcc-');
-    
+
     // Передаем данные в скрипт через data-атрибуты
     $data_attributes = 'data-github-profile="' . esc_attr($github_profile) . '" data-container-id="' . esc_attr($unique_id) . '"';
-    
+
     return '<div class="github-commit-chart-container" id="' . esc_attr($unique_id) . '" ' . $data_attributes . '>
                 <div class="github-commit-chart-loading">Загрузка диаграммы коммитов...</div>
             </div>';
