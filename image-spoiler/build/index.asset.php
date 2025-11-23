0d864f9d2ea82eedbd498656733a9c0e18a0095f
Vladimir Bychko <vladimir@bychko.ru>
2025-11-23 16:33:38 +0200

    Исправлены ошибки: 1.	Изображение под спойлером должно сохранять размер и выравнивание. Сейчас оно выравнивается по левой стороне экрана. Влияй стилями только на размытие. 2.	Размытие не работает.

--- image-spoiler/build/index.asset.php
+++ image-spoiler/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'feb96037aad888fdff7d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e8a50367005d0ffe3a57');
--- image-spoiler/image-spoiler.php
+++ image-spoiler/image-spoiler.php
@@ -3,7 +3,7 @@
  * Plugin Name: Image Spoiler
  * Plugin URI: http://bychko.ru
  * Description: Добавляет возможность размытия изображений в блоке Gutenberg с опциональным текстом предупреждения
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Владимир Бычко
  * Author URI: http://bychko.ru
  * License: GPL v2 or later
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Константы плагина
-define('IMAGE_SPOILER_VERSION', '1.0.0');
+define('IMAGE_SPOILER_VERSION', '1.0.1');
 define('IMAGE_SPOILER_PATH', plugin_dir_path(__FILE__));
 define('IMAGE_SPOILER_URL', plugin_dir_url(__FILE__));
 
@@ -100,6 +100,85 @@ class Image_Spoiler {
         );
     }
 }
+
+/**
+ * Модификация вывода блока Image для добавления спойлера
+ *
+ * @param string $block_content Содержимое блока
+ * @param array $block Данные блока
+ * @return string Модифицированное содержимое
+ */
+function image_spoiler_render_block($block_content, $block) {
+    // Проверяем, что это блок Image
+    if ('core/image' !== $block['blockName']) {
+        return $block_content;
+    }
+
+    // Проверяем, включен ли спойлер
+    if (empty($block['attrs']['isSpoiler'])) {
+        return $block_content;
+    }
+
+    // Получаем текст спойлера
+    $spoiler_text = isset($block['attrs']['spoilerText']) ? $block['attrs']['spoilerText'] : '';
+
+    // Используем DOMDocument для корректной модификации HTML
+    $dom = new DOMDocument('1.0', 'UTF-8');
+    // Подавляем предупреждения о невалидном HTML
+    libxml_use_internal_errors(true);
+
+    // Добавляем мета-теги для правильной работы с UTF-8
+    $dom->loadHTML('<?xml encoding="UTF-8">' . $block_content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+    libxml_clear_errors();
+
+    // Находим элемент img
+    $images = $dom->getElementsByTagName('img');
+
+    if ($images->length === 0) {
+        return $block_content;
+    }
+
+    $img = $images->item(0);
+
+    // Добавляем класс спойлера к изображению
+    $current_class = $img->getAttribute('class');
+    $new_class = trim($current_class . ' image-spoiler');
+    $img->setAttribute('class', $new_class);
+
+    // Находим figure (обертку блока)
+    $figures = $dom->getElementsByTagName('figure');
+
+    if ($figures->length > 0) {
+        $figure = $figures->item(0);
+
+        // Добавляем класс к figure
+        $current_figure_class = $figure->getAttribute('class');
+        $new_figure_class = trim($current_figure_class . ' has-image-spoiler');
+        $figure->setAttribute('class', $new_figure_class);
+
+        // Если есть текст спойлера, добавляем его
+        if (!empty($spoiler_text)) {
+            $text_div = $dom->createElement('div');
+            $text_div->setAttribute('class', 'image-spoiler-text');
+            $text_div->setAttribute('aria-hidden', 'true');
+            $text_node = $dom->createTextNode($spoiler_text);
+            $text_div->appendChild($text_node);
+
+            // Добавляем текст после img
+            $img->parentNode->insertBefore($text_div, $img->nextSibling);
+        }
+    }
+
+    // Возвращаем модифицированный HTML
+    $output = $dom->saveHTML();
+
+    // Убираем добавленные теги
+    $output = str_replace('<?xml encoding="UTF-8">', '', $output);
+
+    return $output;
+}
+
+add_filter('render_block', 'image_spoiler_render_block', 10, 2);
 
 // Инициализация плагина
 new Image_Spoiler();
