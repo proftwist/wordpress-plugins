385ded546e42734ceca0ca2ba3bfc434e484fdf9
Vladimir Bychko <vladimir@bychko.ru>
2025-11-27 05:27:01 +0200

    text-with-side-image: промежуточный коммит (плагин работает криво)

--- text-with-side-image/build/index.asset.php
+++ text-with-side-image/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'f59b1a24a3e70c176560');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '5945b2a8cdfecc7564f7');
